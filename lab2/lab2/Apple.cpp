39fc4b83cdec05adf7f9976b01d92c74d81c53f6
kill-bcv <127349025+kill-bcv@users.noreply.github.com>
2023-12-12 11:13:40 +0200

    Add files via upload

---
+++ lab2/lab2/Apple.cpp
@@ -0,0 +1,10 @@
+#include "Apple.h"
+
+
+void Apple::gen_apple()
+{
+    pos.X = (rand() % WIDTH - 1) + 1;
+    pos.Y = (rand() % HEIGHT - 1) + 1;
+}
+
+COORD Apple::get_pos() { return pos; }
---
+++ lab2/lab2/Snake.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <windows.h>
+#include <vector>
+
+#define WIDTH 50
+#define HEIGHT 25
+
+using namespace std;
+
+class Snake
+{
+private:
+    COORD pos;
+    int vel;
+    char dir;
+    int len;
+    vector<COORD> body;
+
+public:
+    Snake(COORD pos, int vel);
+
+    void grow();
+    void move_snake();
+    void direction(char dir);
+
+    vector<COORD> get_body();
+
+    bool collided();
+    bool eaten(COORD food);
+
+    COORD get_pos();
+};
